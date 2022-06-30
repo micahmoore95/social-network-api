6473f710cfe3c2793db3d9490a1d0eaeb46e5685
Moore <mooreart95@gmail.com>
2022-06-30 10:47:32 -0600

    thought routes

---
+++ routes/thought-routes.js
@@ -0,0 +1,61 @@
+const { Thought, User } = require("../models");
+const router = require("express").Router();
+
+
+router.post("/create/:userId", ({ params, body }, res) => {
+  Thought.create(body)
+    .then(({ _id }) => {
+      return User.findOneAndUpdate(
+        { _id: params.userId },
+        { $push: { thoughts: _id } },
+        { new: true }
+      );
+    })
+    .then((dbThoughtData) => res.json(dbThoughtData))
+    .catch((err) => res.status(400).json(err));
+});
+
+router.get("/all", (req, res) => {
+  Thought.findAll({})
+    .populate({
+      path: "reactions",
+      select: "-__v",
+    })
+    .select("-__v")
+    .sort({ _id: -1 })
+    .then((dbThoughtData) => {
+      res.json(dbThoughtData);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/:id", ({ params }, res) => {
+  Thought.findOne({ _id: params.id })
+    .populate({
+      path: "reactions",
+      select: "-__v",
+    })
+    .select("-__v")
+    .then((dbThoughtData) => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: "No thought found with this id!" });
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch((err) => res.status(400).json(err));
+});
+
+router.post("/:thoughtId/reactions", ({ body, params }, res) => {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true }
+    )
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => res.status(400).json(err));
+  });
+
+  module.exports = router;
